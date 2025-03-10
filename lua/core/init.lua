63d7d39848e696d7db2ed9a70871150769583244
Waqar Ahmed <waqarshujrah@gmail.com>
2025-03-10 21:02:13 +0000

    almost almost there...

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -5,14 +5,14 @@ require("core.options")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -27,60 +27,59 @@ require("lazy").setup("plugins")
 --- If there is a `configs.lang.ft`, load it and call `.config()` on it.
 local function try_load_lang_config(ft) end
 vim.api.nvim_create_autocmd("BufRead", {
-    pattern = "*",
-    callback = function()
-        local ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local name = "configs.lang." .. ft
-        if not package.loaded[name] then
-            local success, module = pcall(require, name)
-            if success then
-                print(name .. " loaded")
-                module.config()
-            end
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		local ft = vim.api.nvim_buf_get_option(0, "filetype")
+		local name = "configs.lang." .. ft
+		if not package.loaded[name] then
+			local success, module = pcall(require, name)
+			if success then
+				print(name .. " loaded")
+				module.config()
+			end
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "*",
-    callback = function()
-        local bg = vim.opt.background:get()
-        if bg == "dark" then
-            vim.cmd("hi Visual guifg=Black guibg=White")
-        elseif bg == "light" then
-            vim.cmd("hi Visual guifg=White guibg=Black")
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		local bg = vim.opt.background:get()
+		if bg == "dark" then
+			vim.cmd("hi Visual guifg=Black guibg=White")
+		elseif bg == "light" then
+			vim.cmd("hi Visual guifg=White guibg=Black")
+		end
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = "*",
-    callback = function()
-        vim.lsp.buf.format()
-        -- print("formatted")
-    end,
+	pattern = "*",
+	callback = function()
+		vim.lsp.buf.format()
+		-- print("formatted")
+	end,
 })
 
 require("core.keymaps")
 require("core.theme")
 
 function file_exists(name)
-    local f = io.open(name, "r")
-    if f ~= nil then
-        io.close(f)
-        return true
-    else
-        return false
-    end
+	local f = io.open(name, "r")
+	if f ~= nil then
+		io.close(f)
+		return true
+	else
+		return false
+	end
 end
 
 --print(vim.fn.getcwd() .. "/nvim.lua")
 if file_exists(vim.fn.getcwd() .. "/nvim.lua") then
-    dofile(vim.fn.getcwd() .. "/nvim.lua")
+	dofile(vim.fn.getcwd() .. "/nvim.lua")
 else
-    --print("nvim.lua not found!")
+	--print("nvim.lua not found!")
 end
-
 
 vim.cmd([[
 augroup highlight_yank
@@ -89,5 +88,6 @@ augroup highlight_yank
 augroup END
 ]])
 
-
 require("mason").setup()
+
+--print(vim.fn.getcwd() .. "/" .. vim.fn.substitute(vim.fn.getcwd(), "^.*/", "", ""))
--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,95 +1,100 @@
 local pluginTable = {}
 
---[[
-
-return {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-        "mfussenegger/nvim-dap",
-        "nvim-neotest/nvim-nio",
-        "leoluz/nvim-dap-go"
-    },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "mfussenegger/nvim-dap",
-        },
-
-        opts = {
-            handlers = {},
-            snsure_installed = { "codelldb" },
-        }
-    },
-
-
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        require("dap-go").setup()
-        require("dapui").setup()
-
-
-
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-
-
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-
-
-
-    end
-
-}
-
-
-
-    ]]
-
 local dapPlugin = {
 
 	"mfussenegger/nvim-dap",
-
 	config = function()
 		local dap = require("dap")
 
 		dap.adapters.lldb = {
 			type = "executable",
-			command = "/usr/bin/lldb",
+			command = "lldb",
+			--command = "/Library/Developer/CommandLineTools/usr/bin/lldbusr/bin/lldb",
 			name = "lldb",
 		}
-
-		dap.configurations.cpp = {
-			{
-				name = "launch",
-				type = "lldb",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to ececutable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				--program = 'program',
-
-				cmd = "$(workspaceFolder)",
-				stopOnEntry = true,
-				args = {},
-			},
-		}
-
-		print("all good upto here!!!")
+		---
+		-----
+		---
+		---
+		--print("all good upto here!!!")
 		vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, {})
 		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+		vim.keymap.set(
+			"n",
+			"<leader>do",
+			"<cmd>lua require'dap'.step_over()<cr>",
+			{ desc = "DAP | Step Over", silent = true }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>di",
+			"<cmd>lua require'dap'.step_into()<cr>",
+			{ desc = "DAP | Step Into", silent = true }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>du",
+			"<cmd>lua require'dap'.step_out()<cr>",
+			{ desc = "DAP | Step Out", silent = true }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>db",
+			"<cmd>lua require'dap'.toggle_breakpoint()<cr>",
+			{ desc = "DAP | Breakpoint", silent = true }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>dB",
+			"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>",
+			{ desc = "DAP | Breakpoint Condition", silent = true }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>dd",
+			"<cmd>lua require'dapui'.toggle()<cr>",
+			{ desc = "DAP | Dap UI", silent = true }
+		)
+
+		vim.keymap.set(
+			"n",
+			"<leader>dl",
+			"<cmd>lua require'dap'.run_last()<cr>",
+			{ desc = "DAP | Run Last", silent = true }
+		)
+
+		require("plugins.dap.codelldb")
+	end,
+}
+
+--
+--
+--
+--
+local masonDapPlugin = {
+
+	"jay-babu/mason-nvim-dap.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"mfussenegger/nvim-dap",
+	},
+
+	opts = {
+		handlers = {},
+		ensure_installed = {
+			"codelldb",
+		},
+	},
+
+	event = "VeryLazy",
+
+	config = function()
+		local dap = require("dap")
 	end,
 }
 
@@ -100,38 +105,34 @@ local dapuiPlugin = {
 		"nvim-neotest/nvim-nio",
 		"leoluz/nvim-dap-go",
 	},
-    
 
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		--require("dap-go").setup()
+		require("dapui").setup()
 
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        --require("dap-go").setup()
-        require("dapui").setup()
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
 
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
 
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
-        end
-
-
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end 
-
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+		--vim.keymap.set("n", "<leader>dw", ":lua require'dapui'.elements.add(vim.fn.expand('<cword><cr>'))", {})
+	end,
 }
 
 table.insert(pluginTable, dapPlugin)
 table.insert(pluginTable, dapuiPlugin)
+--table.insert(pluginTable, masonDapPlugin)
 
 return pluginTable
